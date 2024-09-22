a782bceca8b959963181ef484ed05b6a0ca93527
Svitlana Verkhomii <svitlanaverkhomii@gmail.com>
2024-09-22 21:35:38 +0100

    add task-4

--- js/task-1.js
+++ js/task-1.js
@@ -6,37 +6,37 @@ const users = [
     {
         name: "Moore Hensley",
         email: "moorehensley@indexia.com",
-        balance: 2811,
+        balance: 2811
     },
     {
         name: "Sharlene Bush",
         email: "sharlenebush@tubesys.com",
-        balance: 3821,
+        balance: 3821
     },
     {
         name: "Ross Vazquez",
         email: "rossvazquez@xinware.com",
-        balance: 3793,
+        balance: 3793
     },
     {
         name: "Elma Head",
         email: "elmahead@omatom.com",
-        balance: 2278,
+        balance: 2278
     },
     {
         name: "Carey Barr",
         email: "careybarr@nurali.com",
-        balance: 3951,
+        balance: 3951
     },
     {
         name: "Blackburn Dotson",
         email: "blackburndotson@furnigeer.com",
-        balance: 1498,
+        balance: 1498
     },
     {
         name: "Sheree Anthony",
         email: "shereeanthony@kog.com",
-        balance: 2764,
+        balance: 2764
     },
 ];
 
